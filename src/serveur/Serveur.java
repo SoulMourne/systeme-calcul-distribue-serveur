7fbb6df8ffb680363647565c5a88d1e71236d3c3
Goodwin Jean-Paul <goodwinjeanpaul@yahoo.fr>
2015-12-15 11:28:24 +0100

    Ajout d'une focntionnalité permettant la lecture d'un message provenant du client.

--- src/serveur/Serveur.java
+++ src/serveur/Serveur.java
@@ -1,13 +1,18 @@
 package serveur;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Serveur {
     public static void main(String[] zero) {
+        
         ServerSocket socketserver; //Variable du socket du serveur permettant aux clients de s'y connecter
         Socket socketduserveur = null;  //Initialisation d'un socket pour la communication avec le/les clients
+        BufferedReader in;
+        
         try {
             socketserver = new ServerSocket(2009);  //Initialisation d'un ServerSocket sur le port 2009
             System.out.println("Le serveur est à l'écoute du port "+socketserver.getLocalPort());   //Indique sur quel port le serveur est à l'écoute
@@ -17,6 +22,8 @@ public class Serveur {
                 socketduserveur = socketserver.accept(); //Le Socket attend un socket de la part du client
                 System.out.println("Un client s'est connecté !");   //Le client s'est connecté
 //                String ipClient = socketduserveur.getRemoteSocketAddress().toString()+"\n";   //Récupère l'adresse IP du client
+                in = new BufferedReader (new InputStreamReader (socketduserveur.getInputStream())); //permet de lire les caractères provenant du socketduserveur
+                System.out.println(in.readLine());
                 socketduserveur.close();    //Le socket se ferme
                 i++;    //Le compteur augmente
             }
