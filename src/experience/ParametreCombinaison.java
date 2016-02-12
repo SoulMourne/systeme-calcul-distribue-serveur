f3fefe274251cfd2ead1e9153dfe6fabfca0ed58
Damien Leroy <damien7leroy@gmail.com>
2016-02-12 14:57:46 +0100

    Gestion des paramètres par interval

---
+++ src/experience/ParametreCombinaison.java
@@ -0,0 +1,20 @@
+package experience;
+
+/**
+ * Classe qui permet d'enregistrer les tuples de paramètres à exécuter
+ */
+public class ParametreCombinaison extends Parametre
+{
+	/**
+	 * Combinaison de paramètre à exécuter
+	 */
+	private String [] parametres;
+
+	/**
+	 * Constructeur par défaut
+	 */
+	public ParametreCombinaison()
+	{
+		
+	}
+}
